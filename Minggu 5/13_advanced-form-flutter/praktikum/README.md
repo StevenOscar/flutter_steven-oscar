6b49d12e5da14f5db285ab032e244150532ebe0d
StevenOscar <stevenoscar458@gmail.com>
2023-09-22 22:24:45 +0700

    remove unnecessary print

---
+++ Minggu 5/13_advanced-form-flutter/praktikum/README.md
@@ -0,0 +1,5 @@
+# Panduan Tugas
+
+## IMPORTANT
+
+### Jika ingin membuka halaman untuk tugas prioritas 1, maka uncomment bagian FormPickerPage() pada main.dart
--- Minggu 5/13_advanced-form-flutter/praktikum/advanced_form_project/lib/home_page.dart
+++ Minggu 5/13_advanced-form-flutter/praktikum/advanced_form_project/lib/home_page.dart
@@ -469,14 +469,7 @@ class _HomePageState extends State<HomePage> {
                                 onPressed: () {
                                   _nameController = TextEditingController(text : contactList[index].name);
                                   _numberController = TextEditingController(text : contactList[index].number);
-                                  pickedDateValue = contactList[index].birthDate;
                                   pickedColorValue = contactList[index].profileColor;
-                                  profilePictureFile = contactList[index].profilePicture;
-                                  print("Nama : ${contactList[index].name}");
-                                  print("Nomor Telepon : ${contactList[index].number}");
-                                  print("Tanggal lahir : ${contactList[index].birthDate}");
-                                  print("Warna Profil : ${contactList[index].profileColor}");
-                                  print("Nama File Gambar : ${contactList[index].profilePicture?.name}");
                                   editDialog(index);
                                 }
                               ),
